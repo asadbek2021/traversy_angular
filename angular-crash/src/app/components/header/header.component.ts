0cc592828c1c0113321240e8a5c0238397e4d7e2
Asadbek <asadbekraimov642@gmail.com>
2022-03-27 01:55:45 +0500

    feat: final

--- angular-crash/src/app/components/header/header.component.ts
+++ angular-crash/src/app/components/header/header.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Subscription } from 'rxjs';
+import { UiService } from './../../services/ui.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header',
@@ -7,14 +10,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
   title: string = 'Task Tracker';
+  showAddTask!: boolean;
+  subscrition!: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UiService, private router:Router) {
+    this.subscrition = this.uiService.onToggle().subscribe(value=>{
+      this.showAddTask = value;
+    })
+  }
 
   ngOnInit(): void {
   }
 
   toggleAddTask(): void {
-    console.log('toggleAddTask');
+    this.uiService.toggleAddTask();
+  }
+
+  hasRoute(route: string):boolean{
+    return this.router.url === route;
   }
 
 }
--- angular-crash/src/environments/environment.ts
+++ angular-crash/src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyA66YR_y6ZRzx3Y-Ed2lLq6laFRHTq3kEg",
+    authDomain: "angular-2037c.firebaseapp.com",
+    databaseURL: "https://angular-2037c-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "angular-2037c",
+    storageBucket: "angular-2037c.appspot.com",
+    messagingSenderId: "735704920101",
+    appId: "1:735704920101:web:a49c1138c1000f4af68fcc",
+    measurementId: "G-TRGXRH1BPK",
+  },
 };
 
 /*
